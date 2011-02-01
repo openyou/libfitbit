1be4c31796a204631c025aad4a7b24a395c589d8
Kyle Machulis <kyle@nonpolynomial.com>
2011-02-01 00:05:20 -0800

    Added tracker class since tracker packet is figured out Added more functions for parsing what seems to be fitbit protocol data Reimplemented "transmit" function in as much fitbit protocol detail as possible Lots and lots of comments, few coherent

--- python/fitbit.py
+++ python/fitbit.py
@@ -1,9 +1,84 @@
 #!/usr/bin/env python
-
-# Originally taken from
+#################################################################
+# python fitbit object
+# By Kyle Machulis <kyle@nonpolynomial.com>
+# http://www.nonpolynomial.com
+#
+# Distributed as part of the libfitbit project
+#
+# Repo: http://www.github.com/openyou/libfitbit
+#
+# Licensed under the BSD License, as follows
+#
+# Copyright (c) 2011, Kyle Machulis/Nonpolynomial Labs
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, 
+# with or without modification, are permitted provided 
+# that the following conditions are met:
+#
+#    * Redistributions of source code must retain the 
+#      above copyright notice, this list of conditions 
+#      and the following disclaimer.
+#    * Redistributions in binary form must reproduce the 
+#      above copyright notice, this list of conditions and 
+#      the following disclaimer in the documentation and/or 
+#      other materials provided with the distribution.
+#    * Neither the name of the Nonpolynomial Labs nor the names 
+#      of its contributors may be used to endorse or promote 
+#      products derived from this software without specific 
+#      prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#################################################################
+#
+# ANT code originally taken from
 # http://code.google.com/p/mstump-learning-exercises/source/browse/trunk/python/ANT/ant_twisted.py
-# Added to and untwistedized by Kyle Machulis <kyle@nonpolynomial.com>
+# Added to and untwistedized and basically fixed up by Kyle Machulis <kyle@nonpolynomial.com>
+#
+# What's Done
+#
+# - Basic ANT protocol implementation
+# - Basic FitBit protocol implementation
+# - FitBit Base Initialization
+# - FitBit Tracker Connection, Initialization, Info Retreival
+# - Blind data retrieval (can get it, don't know what it is)
+#
+# To Do (Big)
+#
+# - Talking to the fitbit website
+# - Dividing out into modules (ant classes may become their own library)
+# - Figuring out more data formats and packets
+# - Implementing data clearing
+# - Fix ANT Burst packet identifer
+# - Add checksum checks for ANT receive
+#
+# WTFs
+#
+# - When sending opcode packets, the usb logs say there're responses,
+# but they don't seem to work right. i.e. we should get more an
+# acknowledge response from the 0x24 opcode command (should get back a
+# packet with 0x42 as data), but it just stalls on read after we
+# receive the confirmation.
+# - Same as above happens with data bank transfers too. Currently
+# there's a 'no_burst' argument to skip often null banks. This is a
+# hack until I have better understanding of the protocol.
 
+
+
+import itertools
 import time
 import base64
 import sys
@@ -112,10 +187,10 @@ class ANT(object):
     def _check_ok_response(self):
         # response packets will always be 7 bytes
         status = self._receive(7)
-        
+
         if status[2] == 0x40 and status[5] == 0x0:
             return
-        
+
         raise ANTStatusException("Message status %d does not match 0x0 (NO_ERROR)" % (status[5]))
 
     def reset(self):
@@ -158,6 +233,14 @@ class ANT(object):
         self._send_message(0x42, self._chan, 0x00, 0x00)
         self._check_ok_response()
 
+    def receive_acknowledged_reply(self, size = 13):        
+        while 1:
+            status = self._receive(4096, True)
+            if len(status) > 0 and status[2] == 0x4F and status[5] == 0x5:
+                if self._debug:
+                    self.data_received(''.join(map(chr, status)))
+                return status[4:-1]
+
     def _check_acknowledged_response(self):
         # response packets will always be 7 bytes
         while 1:
@@ -169,17 +252,18 @@ class ANT(object):
             #time.sleep(1)
 
     def _check_burst_response(self):
-        # response packets will always be 7 bytes
+        response = []
         while 1:
             status = self._receive(4096, True)
             if len(status) > 0 and status[2] == 0x50:
                 if self._debug:
                     self.data_received(''.join(map(chr, status)))
+                response = response + status[4:-1].tolist()
                 if status[3] == 0xc0 or status[3] == 0xe0 or status[3] == 0xa0:
-                    return 0
+                    return response
 
     def send_acknowledged_data(self, l):
-        self._send_message(0x4f, self._chan, l)        
+        self._send_message(0x4f, self._chan, l)
         self._check_acknowledged_response()
 
     def send_str(self, instring):
@@ -197,7 +281,6 @@ class ANT(object):
                 data.append(l)
         data.insert(0, len(data) - 1)
         data.insert(0, 0xa4)
-        # self._transmitBuffer = map(lambda i: struct.pack('!H', i)[1], array.array('B', data + [reduce(operator.xor, data)]))
         self._transmitBuffer = map(chr, array.array('B', data + [reduce(operator.xor, data)]))
 
         if self._debug:
@@ -235,9 +318,6 @@ class ANTlibusb(ANT):
     def _send(self, command):
         # libusb expects ordinals, it'll redo the conversion itself.
         c = command
-        # if ord(c[2]) == 0x4f:
-        #     c = c + ['\x00', '\x00']
-        # print c
         self._connection.write(self.ep['out'], map(ord, c), 0, 100)
 
     def _receive(self, size=64, quiet=False):
@@ -246,9 +326,62 @@ class ANTlibusb(ANT):
             self.data_received(''.join(map(chr, r)))
         return r
 
+
 class FitBit(ANTlibusb):
+
+    class FitBitTracker(object):
+        def __init__(self):
+            # Cycle of 0-8, for creating tracker packet serial numbers
+            self.tracker_packet_count = itertools.cycle(range(0,8))
+            
+            # The tracker expects to start on 1, i.e. 0x39 This is set
+            # after a reset (which is why we create the tracker in the
+            # reset function). It won't talk if you try anything else.
+            self.tracker_packet_count.next()
+            
+            self.current_packet_id = None
+            self.serial = None
+            self.firmware_version = None
+            self.bsl_major_version = None
+            self.bsl_minor_version = None
+            self.app_major_version = None
+            self.app_minor_version = None
+            self.in_mode_bsl = None
+            self.on_charger = None
+
+        def get_packet_id(self):
+            self.current_packet_id = 0x38 + self.tracker_packet_count.next()
+            return self.current_packet_id
+
+        def parse_info_packet(self, data):
+            self.serial = data[0:5]
+            self.firmware_version = data[5]
+            self.bsl_major_version = data[6]
+            self.bsl_minor_version = data[7]
+            self.app_major_version = data[8]
+            self.app_minor_version = data[9]
+            self.in_mode_bsl = (False, True)[data[10]]
+            self.on_charger = (False, True)[data[11]]
+
+        def __str__(self):
+            return "Tracker Serial: %s\n" \
+                "Firmware Version: %d\n" \
+                "BSL Version: %d.%d\n" \
+                "APP Version: %d.%d\n" \
+                "In Mode BSL? %s\n" \
+                "On Charger? %s\n" % \
+                ("".join(["%x" % (x) for x in self.serial]),
+                 self.firmware_version,
+                 self.bsl_major_version,
+                 self.bsl_minor_version,
+                 self.app_major_version,
+                 self.app_minor_version,
+                 self.in_mode_bsl,
+                 self.on_charger)
+
     def __init__(self, debug = False):
         super(FitBit, self).__init__(debug=debug)
+        self.tracker = None
 
     def open(self):
         return super(FitBit, self).open(0x10c4, 0x84c4)
@@ -280,17 +413,19 @@ class FitBit(ANTlibusb):
         # seeing it in the USB analyzer logs, but whatever.
         self._check_reset_response()
 
-    def reset_fitbit(self):
+    def reset_tracker(self):
         # 0x78 0x01 is apparently the device reset command
         self.send_acknowledged_data([0x78, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
 
+    def init_tracker_for_transfer(self):
         # 0x78 0x02 is device id reset. This tells the device the new
         cid = [random.randint(0,254), random.randint(0,254)]
         self.send_acknowledged_data([0x78, 0x02] + cid + [0x00, 0x00, 0x00, 0x00])
         self.close_channel()
         self.init_device_channel(cid + [0x01, 0x01])
         self.wait_for_beacon()
-
+        self.tracker = self.FitBitTracker()
+        
     def wait_for_beacon(self):
         # FitBit device initialization
         while 1:
@@ -301,7 +436,7 @@ class FitBit(ANTlibusb):
             except usb.USBError:
                 pass
 
-    def init_device_channel(self, channel): 
+    def init_device_channel(self, channel):
         # ANT device initialization
         self.reset()
         self.send_network_key(0, [0,0,0,0,0,0,0,0])
@@ -327,52 +462,109 @@ class FitBit(ANTlibusb):
         self._connection.ctrl_transfer(0x80, 0x06, 0x0303, 0x0409, 0x01fe)
         self._connection.ctrl_transfer(0x80, 0x06, 0x0302, 0x0409, 0x01fe)
 
-    def transfer(self):
+    def _get_tracker_burst(self):
+        d = self._check_burst_response()
+        if d[1] != 0x81:
+            raise Exception("Response received is not tracker burst! Got %s" % (d[0:2]))
+        size = d[2]
+        if size == 0:
+            return []
+        return d[8:8+size]
+
+    def send_tracker_control_packet(self, index):
+        self.send_tracker_packet([0x70, 0x00, 0x02, index, 0x00, 0x00, 0x00])
+        return self._get_tracker_burst()
+
+    def get_tracker_info(self):
+        # Device data retrieval
+        self.tracker.parse_info_packet(self.send_tracker_control_packet(0x0))
+        print self.tracker
+
+    def send_tracker_packet(self, packet, expected_response = None):
+        self.send_acknowledged_data([self.tracker.get_packet_id()] + packet)
+        # if expected_response is not None:
+        #     self.check_tracker_response(expected_response)
+
+    def check_tracker_response(self, response):
+        data = self.receive_acknowledged_reply()
+        if data[0] != self.tracker.current_packet_id:
+            raise Exception("Tracker Packet IDs don't match! %s %s", (data[0], self.tracker.current_packet_id))
+        if data[1:] != response:
+            raise Exception("Tracker Packet responses don't match! %s %s", (data[1:], response))
+
+    def ping_tracker(self):
         self.send_acknowledged_data([0x78, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
-        self.send_acknowledged_data([0x39, 0x24, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
-        self.send_acknowledged_data([0x3A, 0x70, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00])
-        # Static. Should equal:
-        # ['a4', '09', '50', '00', '3a', '81', '0c', '00', '00', '00', '00', 'ea', 'a0']
-        # ['a4', '09', '50', '20', '58', 'f7', '7a', '65', '51', '0a', '02', '17', '23']
-        # ['a4', '09', '50', 'c0', '02', '17', '00', '01', '1e', '5f', '00', '00', '68']
-        self._check_burst_response()
-        self.send_acknowledged_data([0x3B, 0x60, 0x00, 0x02, 0x01, 0x00, 0x00, 0x00])
-        self.send_acknowledged_data([0x3C, 0x22, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00])
-        self.send_acknowledged_data([0x3D, 0x60, 0x00, 0x02, 0x02, 0x00, 0x00, 0x00])
+
+    def check_tracker_data_bank(self, index, no_burst = False):
+        self.send_tracker_packet([0x60, 0x00, 0x02, index, 0x00, 0x00, 0x00])
+        # HACK. Dunno why, some banks don't send back that they don't
+        # have anything to send. This doesn't match the usb logs.
+        if not no_burst:
+            return self._get_tracker_burst()
+        
+    def transfer(self):
+        # Goal of the transfer function: None of these should be
+        # "send_tracker_packet" calls. Everything should be named what
+        # it actually is.
+        
+        # This is a replay of commands taken from the windows client
+        # logs.
+
+        # URL REQUEST HERE
+        # This is where we get the first opcode for pinging
+
+        # Ping Command
+        self.ping_tracker()
+        self.send_tracker_packet([0x24, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+                                 [0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+
+        # Get tracker info starts us at 0x0. Each call increases by
+        # one, be it to a databank or a configuration query.
+        self.get_tracker_info()
+
+        # There is never anything in bank 1. I don't even know why we check it.
+        self.check_tracker_data_bank(0x1, True)
+
+        # URL REQUEST HERE
+        # This is where we get the rest of the opcodes to query
+        
+        self.send_tracker_packet([0x22, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00])
+
+        # There is probably data coming back from this I need.
+        data = self.send_tracker_control_packet(0x2)
+        self.check_tracker_data_bank(0x3, True)
+
         # Changes - does not match usb log!
         # ['a4', '09', '50', '00', '3d', '81', '0e', '00', '00', '00', '00', 'XX', 'XX']
         # ['a4', '09', '50', '20', 'XX', '00', 'XX', '00', 'XX', '00', 'XX', '00', 'XX']
-        # ['a4', '09', '50', 'c0', '1e', '1e', 'XX', '10', '1e', '5f', '00', '00', 'XX']        
-        self._check_burst_response()
-        self.send_acknowledged_data([0x3E, 0x60, 0x00, 0x02, 0x03, 0x00, 0x00, 0x00])
-        self.send_acknowledged_data([0x3F, 0x22, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00])
-        self.send_acknowledged_data([0x38, 0x60, 0x00, 0x02, 0x04, 0x00, 0x00, 0x00])
-        self._check_burst_response()
-        self.send_acknowledged_data([0x39, 0x60, 0x00, 0x02, 0x05, 0x00, 0x00, 0x00])
-        self.send_acknowledged_data([0x3A, 0x22, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00])
-        self.send_acknowledged_data([0x3B, 0x60, 0x00, 0x02, 0x06, 0x00, 0x00, 0x00])
-        self._check_burst_response()
-        self.send_acknowledged_data([0x3C, 0x60, 0x00, 0x02, 0x07, 0x00, 0x00, 0x00])
-        self._check_burst_response()
-        self.send_acknowledged_data([0x3D, 0x60, 0x00, 0x02, 0x08, 0x00, 0x00, 0x00])
-        self._check_burst_response()
-        self.send_acknowledged_data([0x3E, 0x60, 0x00, 0x02, 0x09, 0x00, 0x00, 0x00])
-        self.send_acknowledged_data([0x3F, 0x22, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
-        self.send_acknowledged_data([0x38, 0x70, 0x00, 0x02, 0x0A, 0x00, 0x00, 0x00])
-        self._check_burst_response()
-        self.send_acknowledged_data([0x39, 0x60, 0x00, 0x02, 0x0B, 0x00, 0x00, 0x00])
-        self.send_acknowledged_data([0x3A, 0x22, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00])
-        self.send_acknowledged_data([0x3B, 0x70, 0x00, 0x02, 0x0C, 0x00, 0x00, 0x00])
-        self._check_burst_response()
-        # self.send_acknowledged_data([0x3C, 0x60, 0x00, 0x02, 0x0D, 0x00, 0x00, 0x00])
-        # self.send_acknowledged_data([0x3D, 0x26, 0xC3, 0x3B, 0x46, 0x4D, 0x00, 0x00])
-        # self.send_acknowledged_data([0x3A, 0x23, 0x00, 0x0E, 0x00, 0x00, 0x00, 0x00])
+        # ['a4', '09', '50', 'c0', '1e', '1e', 'XX', '10', '1e', '5f', '00', '00', 'XX']
+
+        self.send_tracker_packet([0x22, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00])
+        self.check_tracker_data_bank(0x04)
+        self.check_tracker_data_bank(0x05, True)
+        self.send_tracker_packet([0x22, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00])
+        self.check_tracker_data_bank(0x06)
+        self.check_tracker_data_bank(0x07)
+        self.check_tracker_data_bank(0x08)
+        self.check_tracker_data_bank(0x09, True)
+        self.send_tracker_packet([0x22, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+        data = self.send_tracker_control_packet(0x0A)
+        self.check_tracker_data_bank(0x0B, True)
+        self.send_tracker_packet([0x22, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00])
+        data = self.send_tracker_control_packet(0x0C)
+        self.check_tracker_data_bank(0x0D, True)
+
+        # URL REQUEST HERE
+        # This is where we ship everything back to the mothership
+
+        # somewhere down here, we would erase data. If only we knew how.
 
     def run_transfer(self):
         self._connection.reset()
         self.init_fitbit()
         self.wait_for_beacon()
-        self.reset_fitbit()
+        self.reset_tracker()
+        self.init_tracker_for_transfer()
         self.transfer()
 
     def run_opcode(self, opcode):
