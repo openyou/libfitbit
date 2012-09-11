29459fe9eec04279af59d3e0d68144d8e8ebec46
Theodore Ts'o <tytso@mit.edu>
2012-09-11 18:18:56 -0400

    Immediately restart looking for a new client after finishing a cycle
    Now that we are putting the fitbit device to sleep for 15 minutes, it
    is not necessary for the fitbit_client.py script to sleep for 15
    minutes before restarting the cycle.
    
    If there are multiple devices in range of the base station, this is
    important so that the fitbit client can quickly collect information
    from both devices.  It is also useful in the situation where someone
    wanders by the base station software while it is running; by not
    making the base station software be quiscient for 15 minutes between
    polls, it makes it more likely that we will collect information from a
    fitbit client which is near us, and send that information back to the
    fitbit servers.
    
    (This is especially handy if there are going to be large number of
    users at a site all using fitbits as part of a employee fitness
    program.)
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- python/fitbit_client.py
+++ python/fitbit_client.py
@@ -179,8 +179,7 @@ if __name__ == '__main__':
             print '-'*60
         else:
             print "normal finish"
-            print time.ctime(), "waiting", cycle_minutes, "minutes and then restarting..."
-            time.sleep(60*cycle_minutes)
+            print "restarting..."
     
     #sys.exit(main())
 
